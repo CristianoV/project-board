34b7a4dc863b8488e800627062517c8904707483
CristianoV <cristianoviieira@gmail.com>
2023-01-04 13:44:40 -0300

    feat: adiciona novas rotas e estilização ao header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,20 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import style from './styles.module.scss';
+import { SignInButton } from '../SignInButton';
 
 export function Header() {
   return (
     <header className={style.headerContainer}>
       <div className={style.headerContent}>
         <Link href='/'>
-          <Image src='/images/logo.svg' alt='Logo' width={100} height={60} />
+          <Image src='/images/logo.svg' alt='Logo' width={72} height={76} />
         </Link>
         <nav>
-          <Link href='/'>
-            Home
-          </Link>
-          <Link href='/board'>
-            Meu board
-          </Link>
+          <Link href='/'>Home</Link>
+          <Link href='/board'>Meu board</Link>
         </nav>
-        <button>Entrar com github</button>
+        <SignInButton />
       </div>
     </header>
   );

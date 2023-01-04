f8b1f4341acb5566470bef095547c1a0beea178b
CristianoV <cristianoviieira@gmail.com>
2023-01-03 22:15:15 -0300

    feat: pagina inicial criada

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Board - Organizando suas tarefas.</title>
+      </Head>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </>
+  );
+}

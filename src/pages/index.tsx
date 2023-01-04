413802a58795bde6064f07cca618f2120470bfd3
CristianoV <cristianoviieira@gmail.com>
2023-01-04 13:45:21 -0300

    feat: adiciona conteudo na pagina principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import style from '../styles/styles.module.scss';
 
 export default function Home() {
   return (
@@ -6,9 +7,24 @@ export default function Home() {
       <Head>
         <title>Board - Organizando suas tarefas.</title>
       </Head>
-      <div>
-        <h1>Home</h1>
-      </div>
+      <main className={style.contentContainer}>
+        <img src='images/board-user.svg' alt='Board' />
+        <section className={style.callToAction}>
+          <h1>
+            Uma ferramenta para o seu dia a dia Escreva, planeje e organize-se..
+          </h1>
+          <p>
+            <span>100% Gratuita</span> e online.
+          </p>
+        </section>
+
+        <div className={style.donaters}>
+          <img
+            src='https://avatars.githubusercontent.com/u/12345678?v=4'
+            alt='Ilustração'
+          />
+        </div>
+      </main>
     </>
   );
 }

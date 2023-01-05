c6646f99dc7d388c44b28fdc185b043405948cb2
CristianoV <cristianoviieira@gmail.com>
2023-01-05 15:53:18 -0300

    feat: adiciona configurações do firebase

---
+++ src/services/firebaseConnection.ts
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import 'firebase/firestore';
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export const add = async (path: string, data: Object) => {
+  try {
+    return await addDoc(collection(db, path), data);
+  } catch (error) {
+    console.error('Error adding document: ', error);
+  }
+};
+
+export default firebase;

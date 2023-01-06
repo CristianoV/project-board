56f5672eb831a84c40257bf68850f569d4f290e7
CristianoV <cristianoviieira@gmail.com>
2023-01-06 17:36:21 -0300

    feat: adiciona funções para alterar banco de dados

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -39,4 +39,60 @@ export const add = async (path: string, data: Object) => {
   }
 };
 
+export const get = async (path: string, userId: string) => {
+  try {
+    const postQuery = query(
+      collection(db, path),
+      where('userId', '==', userId),
+      orderBy('created', 'desc')
+    );
+
+    const querySnapshot = await getDocs(postQuery);
+
+    return querySnapshot.docs.map((doc) => {
+      return { id: doc.id, ...doc.data() };
+    }, []);
+  } catch (error) {
+    console.error('Error adding document: ', error);
+  }
+};
+
+export const Remove = async (path: string, id: string) => {
+  try {
+    const docRef = doc(db, path, id);
+    await deleteDoc(docRef);
+  } catch (error) {
+    console.error('Error adding document: ', error);
+  }
+};
+
+export const Edit = async (path: string, id: string, data: any) => {
+  try {
+    const docRef = doc(db, path, id);
+    await updateDoc(docRef, data);
+  } catch (error) {
+    console.error('Error adding document: ', error);
+
+    return error;
+  }
+};
+
+export const getOne = async (path: string, id: string) => {
+  try {
+    const docRef = doc(db, path, id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return {
+        id: docSnap.id,
+        createdFormated: format(new Date(), 'dd MMMM yyyy', { locale: ptBR }),
+        ...docSnap.data(),
+      };
+    } else {
+      console.log('No such document!');
+    }
+  } catch (error) {
+    console.error('Error adding document: ', error);
+  }
+};
 export default firebase;

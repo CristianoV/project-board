021fae9b5b0fef606eb30198db0265a21d27df85
CristianoV <cristianoviieira@gmail.com>
2023-01-07 16:35:32 -0300

    feat: adiciona provider do paypal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-
 import '../styles/global.scss';
 import { Header } from '../components/Header';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+
+const initialOptions = {
+  'client-id':
+    'AWNeUfBkVz4dkKDIUncQgY6t4-Bmz5DCBI6h1HH1fHKSqJxPiGIhUlnHPUJVIJZj37K9iBjBJCvqc3Gy',
+  currency: 'BRL',
+  intent: 'capture',
+};
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <SessionProvider session={pageProps.session}>
-        <Header />
-        <Component {...pageProps} />
+        <PayPalScriptProvider options={initialOptions}>
+          <Header />
+          <Component {...pageProps} />
+        </PayPalScriptProvider>
       </SessionProvider>
     </>
   );
